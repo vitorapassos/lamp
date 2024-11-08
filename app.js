ea426a1ec7b4d994d0af87eebaafa5872b5e089d
VITOR DE ASSIS PASSOS <vitor.apassos@senacsp.edu.br>
2024-11-08 19:55:25 -0300

    Atualizados

--- app.js
+++ app.js
@@ -3,16 +3,22 @@
  * @author Vitor de Assis
  */
 
+
+// Variáveis de apoio a lógica
 let quebrada = false;
 
+// Pré carregamento do arquivo de áudio
+let som = new Audio("sound/breaking-glass.mp3")
+//som.src = "sound/glassbreaking.wav"
+
 function quebrar() {
+    // Verificar se a lampada está quebrada antes de "quebrar"
     if (quebrada === false) {
         document.getElementById('lamp').src = "img/broken.jpg"
-        //reproduzindo um arquivo de áudio no JS
-        //Passo 1: copiar o arquivo de aúdio para o projeto
-        //Passo 2: Usar a classe Audio(biblioteca interna do JS) 
-        let som = new Audio()
-        som.src = "sound/glassbreaking.wav"
+        // Reproduzindo um arquivo de áudio no JS
+        // Passo 1: copiar o arquivo de aúdio para o projeto
+        // Passo 2: Usar a classe Audio(biblioteca interna do JS) 
+        // Passo 3: Pré carregar o arquivo de áudio para sincronizar com a troca de imagem (UX)
         som.play()
         quebrada = true;
     }
@@ -22,9 +28,9 @@ function onoff() {
     let interruptor2 = document.getElementById('interruptor')
 
     if (quebrada === true) {
-        if (quebrada === true && interruptor2.src === 'http://127.0.0.1:5500/img/swoff.png' ) {
+        if (quebrada === true && interruptor2.src === 'http://127.0.0.1:5500/img/swoff.png') {
             document.getElementById('interruptor').src = "img/swon.png"
-        } else if( quebrada === true) {
+        } else if (quebrada === true) {
             document.getElementById('interruptor').src = "img/swoff.png"
         }
     } else {
